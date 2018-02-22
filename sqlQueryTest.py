32d1738b918b6c6199784a414abe01c92fafd0dc
Robert <the2boys10@gmail.com>
2018-02-22 22:16:47 +0000

    Added new column needs to be counted as count function dosn't work
    On my machine count function not working could be faulty spark installation ect.

--- sqlQueryTest.py
+++ sqlQueryTest.py
@@ -1,9 +1,9 @@
+#
 def splitOnDate(startDate, endDate, TrainingData, TestData):
-    df = spark.sql("SELECT * from temp where CAST(CONCAT( Year, '-', Month, '-', Day, ' ', Hour ) as timestamp) BETWEEN '"+startDate+"' AND '"+endDate+"'")
+    df = spark.sql("SELECT * from temp where TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"'").show(100)
 
 import pyspark
 from pyspark.sql import SQLContext
-
 # Create spark context and sparkSQL objects
 sc = pyspark.SparkContext.getOrCreate()
 spark = SQLContext(sc)
@@ -13,14 +13,11 @@ df = (spark.read
         .format("com.databricks.spark.csv")
         .option("header", "true")
         .load("data/elec_demand_hourly.csv"))
-
 df.registerTempTable("temp")
-
-# Print first ten rows (0 = header)
-df.show(10)
-
-# SQL query and print
-df2 = spark.sql("SELECT Demand from temp WHERE Hour >= 5 AND Day <= 4").show()
+df = spark.sql("SELECT *, CAST(CONCAT( Year, '-', Month, '-', Day, ' ', Hour ) as timestamp) as TimeAndDate from temp")
+df.registerTempTable("temp")
 splitOnDate("2018-02-11 12:00:00","2018-02-11 13:00:00", 80, 20)
+# Print first ten rows (0 = header)
+# SQL query and print
 # Ensure previous spark context has closed (Will fix this)
 sc.stop()
