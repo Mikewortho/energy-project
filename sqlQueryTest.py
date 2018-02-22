e2f5400d682ba24fd2eb9a57eff6f3ea6385add1
Robert <the2boys10@gmail.com>
2018-02-22 17:06:12 +0000

    test
    test

--- sqlQueryTest.py
+++ sqlQueryTest.py
@@ -1,3 +1,6 @@
+def splitOnDate(startDate, endDate, TrainingData, TestData):
+    df = spark.sql("SELECT * from temp where CAST(CONCAT( Year, '-', Month, '-', Day, ' ', Hour ) as timestamp) BETWEEN '"+startDate+"' AND '"+endDate+"'")
+
 import pyspark
 from pyspark.sql import SQLContext
 
@@ -18,6 +21,6 @@ df.show(10)
 
 # SQL query and print
 df2 = spark.sql("SELECT Demand from temp WHERE Hour >= 5 AND Day <= 4").show()
-
+splitOnDate("2018-02-11 12:00:00","2018-02-11 13:00:00", 80, 20)
 # Ensure previous spark context has closed (Will fix this)
 sc.stop()
