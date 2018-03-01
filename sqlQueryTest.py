534364993ed5e784ed7fdbac8cbd47c74676be93
VGMTaylor <32782658+VGMTaylor@users.noreply.github.com>
2018-03-01 09:25:18 +0000

    Update sqlQueryTest.py

--- sqlQueryTest.py
+++ sqlQueryTest.py
@@ -1,9 +1,28 @@
-#
-def splitOnDate(startDate, endDate, TrainingData, TestData):
-    df = spark.sql("SELECT * from temp where TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"'").show(100)
-
 import pyspark
 from pyspark.sql import SQLContext
+
+# Split data into test and training (TODO speedup with indexing query instead)
+def trainingSplit(trainingRatio, fullData):
+    count = fullData.count()
+    train_count = int(round((count * trainingRatio)))
+    training_data = fullData.limit(train_count)
+    test_data = fullData.subtract(training_data)
+    return training_data, test_data
+
+# Split data into k folds (TODO speedup with indexing query instead)
+def foldsSplit(k, fullData):
+    count = fullData.count()
+    pivot = int(round((count / k)))  
+    folds = []
+    for i in range(k):
+        if(i == 0):
+            folds.append(fullData.limit(pivot))
+        else:
+            for j in range(len(folds)):
+                temp = fullData.subtract(folds[j])
+            folds.append(temp.limit(pivot))         
+    return folds            
+            
 # Create spark context and sparkSQL objects
 sc = pyspark.SparkContext.getOrCreate()
 spark = SQLContext(sc)
@@ -13,11 +32,11 @@ df = (spark.read
         .format("com.databricks.spark.csv")
         .option("header", "true")
         .load("data/elec_demand_hourly.csv"))
+
+# Register temporary table including DateTime representation of columns
 df.registerTempTable("temp")
-df = spark.sql("SELECT *, CAST(CONCAT( Year, '-', Month, '-', Day, ' ', Hour ) as timestamp) as TimeAndDate from temp")
+df = spark.sql("SELECT *, CAST(CONCAT( Year, '-', Month, '-', Day, ' ', Hour ) as timestamp) as TimeAndDate from temp ORDER BY TimeAndDate ASC")
 df.registerTempTable("temp")
-splitOnDate("2018-02-11 12:00:00","2018-02-11 13:00:00", 80, 20)
-# Print first ten rows (0 = header)
-# SQL query and print
-# Ensure previous spark context has closed (Will fix this)
+
+
 sc.stop()
