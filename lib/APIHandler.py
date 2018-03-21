f57ca2bcface0d84cbac354db73ae22265151d8c
VGMTaylor <J.A.Taylor2@liv.ac.uk>
2018-03-21 13:40:31 +0000

    Added outlier detection, reading in timestamps to the data, reading in forecast data, Remove EIA and WWA, Merged with robs branched, reformatted code

--- lib/APIHandler.py
+++ lib/APIHandler.py
@@ -18,6 +18,19 @@ class APIHandler:
         d = dict(zip(ba, req))
         d.pop('region', None)
         self.ELEC_DEMAND_DICT = d
+        
+    # Generate dictionary of ba forecasts and their API keys
+    def gen_forecast_dictionary(self):
+        request = 'http://api.eia.gov/category/?api_key=' + self.API_KEY + '&category_id=2122627'
+        r = requests.get(request)
+        x = r.json()
+        ba, req = ([] for i in range(2))
+        for i in range(len(x["category"]["childseries"])) : ba.append(json.dumps(x["category"]["childseries"][i]["name"])), req.append(json.dumps(x["category"]["childseries"][i]["series_id"]))
+        for i in range(len(ba)):
+            ba[i] = ba[i][ba[i].find("(")+1:ba[i].find(")")]
+        d = dict(zip(ba, req))
+        d.pop('region', None)
+        self.ELEC_FORECAST_DICT = d
 
     # Return balancing authority lookup dictionary
     def get_ba_demand_dictionary(self):
@@ -32,6 +45,15 @@ class APIHandler:
             data = [dict(zip(fields, [k, v])) for k, v in self.ELEC_DEMAND_DICT.items()]
             writer.writerows(data)
 
+    # Write ba forecast dictionary to CSV
+    def write_ba_forecast_dictionary(self, FILE_NAME):
+        with open(FILE_NAME, 'w+', newline='') as file:
+            fields = ['Authority', 'Forecast API']
+            writer = csv.DictWriter(file, fieldnames = fields)
+            writer.writeheader()
+            data = [dict(zip(fields, [k, v])) for k, v in self.ELEC_FORECAST_DICT.items()]
+            writer.writerows(data)
+            
     # Generate data frame of hourly electricity demand data
     def gen_ba_demand_table(self):
         hourly_data = []
@@ -40,15 +62,35 @@ class APIHandler:
             r = requests.get(request)
             x = r.json()
             for i in range(len(x["series"][0]["data"])):
-                date = str(x["series"][0]["data"][i][0])
+                date = str(x["series"][0]["data"][i][0]) + ":00:00.000"
                 year = date[:4]
                 month = date[4:6]
                 day = date[6:8]
                 hour = date[9:11]
                 weekday = datetime.date(int(year), int(month), int(day)).weekday()
                 demand = str(x["series"][0]["data"][i][1])
-                hourly_data.append([ba, demand, hour, day, month, year, weekday])
+                if(ba != "EEI" and ba != "WWA"):
+                    hourly_data.append([ba, demand, hour, day, month, year, weekday, date])
         self.ELEC_DEMAND_TABLE = hourly_data;
+        
+    # Generate data frame of hourly forecasting demand data
+    def gen_forecast_table(self):
+        hourly_data = []
+        for ba in self.ELEC_FORECAST_DICT.keys():
+            request = 'http://api.eia.gov/series/?api_key=' + self.API_KEY + '&series_id=' + self.ELEC_FORECAST_DICT[ba].replace('"', '')
+            r = requests.get(request)
+            x = r.json()
+            for i in range(len(x["series"][0]["data"])):
+                date = str(x["series"][0]["data"][i][0]) + ":00:00.000"
+                year = date[:4]
+                month = date[4:6]
+                day = date[6:8]
+                hour = date[9:11]
+                weekday = datetime.date(int(year), int(month), int(day)).weekday()
+                demand = str(x["series"][0]["data"][i][1])
+                if(ba != "EEI" and ba != "WWA"):
+                    hourly_data.append([ba, demand, hour, day, month, year, weekday, date])
+        self.ELEC_FORECAST_TABLE = hourly_data;    
 
     # Return electricity demand data frame
     def get_ba_demand_table(self):
@@ -56,8 +98,16 @@ class APIHandler:
 
     # Write demand table to csv
     def write_ba_demand_table(self, FILE_NAME):
-        fields = ["BA", "Demand", "Hour", "Day", "Month", "Year",  "Weekday"]
+        fields = ["BA", "Demand", "Hour", "Day", "Month", "Year",  "Weekday", "TimeAndDate"]
         with open(FILE_NAME, 'w+') as file:
             writer = csv.writer(file, delimiter=',', lineterminator='\n')
             writer.writerow(fields)
             writer.writerows(self.ELEC_DEMAND_TABLE)
+            
+    # Write forecast table to csv
+    def write_ba_forecast_table(self, FILE_NAME):
+        fields = ["BA", "Demand", "Hour", "Day", "Month", "Year",  "Weekday", "TimeAndDate"]
+        with open(FILE_NAME, 'w+') as file:
+            writer = csv.writer(file, delimiter=',', lineterminator='\n')
+            writer.writerow(fields)
+            writer.writerows(self.ELEC_FORECAST_TABLE)            
--- sqlQueryTest.py
+++ sqlQueryTest.py
@@ -1,5 +1,6 @@
 import pyspark
-from pyspark.sql import SQLContext
+from pyspark.sql.functions import avg, stddev_pop
+from pyspark.sql import SQLContext       
 import zlib
 
 # extracts data between two dates
@@ -8,28 +9,28 @@ def extractDate(startDate, endDate):
 
 #aggragates hourly data into days (Average)(BETWEEN dates)
 def aggtoDay(startDate, endDate):
-    days = spark.sql("SELECT BA, Date, AVG(Demand) TotalDemand from (SELECT BA, Demand, DATE(TimeAndDate) as Date from temp where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"') GROUP BY Date, BA ORDER BY Date" ).repartition(1).write.csv("AggragatedToDays.csv")
+    days = spark.sql("SELECT BA, Date, AVG(Demand) TotalDemand from (SELECT BA, Demand, DATE(TimeAndDate) as Date from demands where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"') GROUP BY Date, BA ORDER BY Date" )#.repartition(1).write.csv("data/AggragatedToDays.csv")
     return days
 
 #aggragates daily data into weeks (Average) (BETWEEN dates)
 def aggtoWeek(startDate, endDate):
-    weeks = spark.sql("SELECT Year(Date) year, weekofyear(Date) week, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from temp where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"') GROUP BY week, year,BA ORDER BY year, week" ).repartition(1).write.csv("AggragatedToWeeks.csv")
+    weeks = spark.sql("SELECT Year(Date) year, weekofyear(Date) week, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from demands where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"') GROUP BY week, year,BA ORDER BY year, week" )#.repartition(1).write.csv("data/AggragatedToWeeks.csv")
     return weeks
 
 def aggtoMonth(startDate, endDate):
-    months = spark.sql("SELECT  Year(Date) year, Month(Date) month, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from temp where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"') GROUP BY month,year,BA ORDER BY year,month" ).repartition(1).write.csv("AggragatedToMonths.csv")
+    months = spark.sql("SELECT  Year(Date) year, Month(Date) month, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from demands where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"') GROUP BY month,year,BA ORDER BY year,month" )#.repartition(1).write.csv("data/AggragatedToMonths.csv")
     return months
 #aggragates weekly data into years (Average) (BETWEEN dates) 
 def aggtoYear(startDate, endDate):
-    years = spark.sql("SELECT  Year(Date) year, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from temp where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"')  GROUP BY year, BA ORDER BY year").repartition(1).write.csv("AggragatedToYears.csv")
+    years = spark.sql("SELECT  Year(Date) year, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from demands where Demand IS NOT NULL AND TimeAndDate BETWEEN '"+startDate+"' AND '"+endDate+"')  GROUP BY year, BA ORDER BY year")#.repartition(1).write.csv("data/AggragatedToYears.csv")
     return years
 
 #performs all aggregations simultaneously (Average) (No date parameters)
 def aggtoDWMY():
-    days = spark.sql("SELECT BA, Date, AVG(Demand) TotalDemand from (SELECT BA, Demand, DATE(TimeAndDate) as Date from temp where Demand IS NOT NULL) GROUP BY Date, BA ORDER BY Date" ).repartition(1).write.csv("AggragatedToDays.csv")
-    weeks = spark.sql("SELECT BA, Year(Date) year, weekofyear(Date) week, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from temp where Demand IS NOT NULL ) GROUP BY week, year,BA ORDER BY year, week" ).repartition(1).write.csv("AggragatedToWeeks.csv")
-    months = spark.sql("SELECT BA, Year(Date) year, Month(Date) month, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from temp where Demand IS NOT NULL ) GROUP BY month,year,BA ORDER BY year,month" ).repartition(1).write.csv("AggragatedToMonths.csv")
-    years = spark.sql("SELECT BA, Year(Date) year, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from temp where Demand IS NOT NULL) GROUP BY year, BA ORDER BY year").repartition(1).write.csv("AggragatedToYears.csv")
+    days = spark.sql("SELECT BA, Date, AVG(Demand) TotalDemand from (SELECT BA, Demand, DATE(TimeAndDate) as Date from demands where Demand IS NOT NULL) GROUP BY Date, BA ORDER BY Date" )#.repartition(1).write.csv("data/AggragatedToDays.csv")
+    weeks = spark.sql("SELECT BA, Year(Date) year, weekofyear(Date) week, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from demands where Demand IS NOT NULL ) GROUP BY week, year,BA ORDER BY year, week" )#.repartition(1).write.csv("data/AggragatedToWeeks.csv")
+    months = spark.sql("SELECT BA, Year(Date) year, Month(Date) month, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from demands where Demand IS NOT NULL ) GROUP BY month,year,BA ORDER BY year,month" )#.repartition(1).write.csv("data/AggragatedToMonths.csv")
+    years = spark.sql("SELECT BA, Year(Date) year, AVG(Demand) TotalDemand from (SELECT BA, Demand, TimeAndDate as Date from demands where Demand IS NOT NULL) GROUP BY year, BA ORDER BY year")#.repartition(1).write.csv("data/AggragatedToYears.csv")
     return days,weeks,months,years
 
 # Split data into test and training (TODO speedup with indexing query instead)
@@ -56,19 +57,39 @@ def foldsSplit(k, fullData):
 
 # Method to get missing dates for each BA, returns a list of format [[Dataframe(of missing dates for BA), BA],[Dataframe(of missing dates for BA), BA]......]
 def getMissingDates():
-    dftemp = spark.sql("Select BA, MAX(TimeAndDate) max ,Min(TimeAndDate) min from temp GROUP BY BA")
+    dftemp = spark.sql("Select BA, MAX(TimeAndDate) max ,Min(TimeAndDate) min from demands GROUP BY BA")
     sample2 = dftemp.take(dftemp.count())
     listOfDataframes = []
     for BA, ENDTIME, STARTTIME in sample2:
         tempList = []
-        tempList.append(spark.sql("Select TimeAndDate from (Select * from temp2 where TimeAndDate<'"+str(ENDTIME)+"' AND TimeAndDate>'"+str(STARTTIME)+"') NATURAL LEFT JOIN (Select BA, TimeAndDate from temp where BA='"+BA+"') AS L WHERE BA IS NULL "))
+        tempList.append(spark.sql("Select TimeAndDate from (Select * from demands where TimeAndDate<'"+str(ENDTIME)+"' AND TimeAndDate>'"+str(STARTTIME)+"') NATURAL LEFT JOIN (Select BA, TimeAndDate from demands where BA='"+BA+"') AS L WHERE BA IS NULL "))
         tempList.append(BA)
         listOfDataframes.append(tempList)
     return listOfDataframes
 
+# Method that returns outliers in lists for each BA, outlier = 4 * std away from mean
+def getOutliers():
+    authorities = spark.sql("SELECT DISTINCT BA from demands").collect()
+    ba_demands = []
+    outliers = []
+    for row in range(len(authorities)):
+        val = authorities[row]
+        ba_demands.append(spark.sql("SELECT Demand, TimeAndDate, BA FROM demands WHERE BA = '%s'" % val))
+    for ba in range(len(ba_demands)):
+        print(str(authorities[ba]) + ":" + str(ba) + " / " +  str(len(ba_demands)))
+        ba_demands[ba].registerTempTable("demand_table")
+        mean = ba_demands[ba].select(avg(ba_demands[0]["Demand"])).collect()
+        std = ba_demands[ba].select(stddev_pop(ba_demands[0]["Demand"])).collect()        
+        lower_bounds = mean[0][0] - 5 * std[0][0]
+        upper_bounds = mean[0][0] + 5 * std[0][0]  
+        o = spark.sql("SELECT Demand, TimeAndDate, BA FROM demand_table WHERE demand > {0} OR demand < {1} OR demand < 0 OR demand = 0".format(upper_bounds, lower_bounds))
+        outliers.append(o)
+    return outliers
+
+# Aggregate into regions
 def regionDataframe():
-    letsSee = spark.sql("Select BA, Name, TimeZone, Region, Name, AVG(Demand) Demand from (Select * from temp natural join temp3) group by BA, Name, TimeZone, Region, Name").show(59)
-    return
+    regions = spark.sql("Select BA, Name, TimeZone, Region, Name, AVG(Demand) Demand from (Select * from demands natural join regions) group by BA, Name, TimeZone, Region, Name").show(59)
+    return regions
 
 #Turn a dataframe containing TimeAndDate, Demand as well as BA into a Json and compress's it using zlib
 def turnDataframeIntoJson(df5):
@@ -80,41 +101,21 @@ def turnDataframeIntoJson(df5):
         sendString += temp2+'\n'
     compressedJson = zlib.compress(sendString.encode())
     return(compressedJson)
-
+    
 # Create spark context and sparkSQL objects
 sc = pyspark.SparkContext.getOrCreate()
 spark = SQLContext(sc) 
 
 # Use sparkSQL to read in CSV
-df = spark.read.csv("newTable.csv", header=True, inferSchema=True)
-df2 = spark.read.csv("temp.csv", header=True, inferSchema=True)
-df3 = spark.read.csv("temp3.csv", header=True, inferSchema=True)
-df3.registerTempTable("temp3")
-df.registerTempTable("temp")
-df2.registerTempTable("temp2")
+demand_table = spark.read.csv("data/elec_demand_hourly.csv", header=True, inferSchema=True)
+date_table = spark.read.csv("data/dates.csv", header=True, inferSchema=True)
+region_table = spark.read.csv("data/region_table.csv", header=True, inferSchema=True)
+forecast_table = spark.read.csv("data/forecast_hourly.csv", header=True, inferSchema=True)
+region_table.registerTempTable("regions")
+demand_table.registerTempTable("demands")
+date_table.registerTempTable("dates")
+forecast_table.registerTempTable("forecasts")
 
-
-
-
-
-
-#regionDataframe()
-#Query = spark.sql("SELECT Year(TimeAndDate), Month(TimeAndDate), weekOfYear(TimeAndDate), Hour(TimeAndDate), Demand from temp where TimeAndDate between '"+startDate+"' and '"+endDate+"' and BA = '"+region+"'")
-#Query.show(10)
-
-
-
-
-
-# Below shows examples of how to use some queries
-
-#splitOnDate("2017-02-11 12:00:00","2018-02-11 13:00:00", 80, 20)
-#aggtoDWMY()
-# Print first ten rows (0 = header)
-#df.show(10)
-
-# SQL query and print
-#df2 = spark.sql("SELECT Demand from temp WHERE Hour >= 5 AND Day <= 4").show()
 
 # Ensure previous spark context has closed (Will fix this)
 sc.stop() 
