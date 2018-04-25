174135cd6aac0ef9ac133e0a35d86bf5e5064362
Robert Johnson <the2boys10@gmail.com>
2018-04-25 19:33:33 +0000

    added colours
    boop

--- Offline Website/js/main.js
+++ Offline Website/js/main.js
@@ -1,9 +1,31 @@
-$(document).ready(function() {
-    $('#map').usmap({
-        stateStyles: {fill: '#95ccff'},
-        stateHoverStyles: {fill: '#e0f4f2'},
-        stateHoverAnimation: 100,
 
+
+function parseData2() {
+    Papa.parse("../data/stateColours.csv", {
+        download: true,
+        complete: function(results) {
+            var resultsData = results.data
+            printMap(resultsData)
+        }
+    })
+}
+$(document).ready(function() {
+    parseData2()
+    console.log("Entered!")
+});
+
+function printMap(data)
+{
+    // Array to object.
+    var myStyles = {}
+    for (var i = 1; i < data.length; i++) {
+        document.getElementsByClassName("main")[0].innerHTML += workOutValues(data[i][1])+"<br\>";
+        myStyles[data[i][0]] = {fill: workOutValues(data[i][1])};
+    }
+
+    $('#map').usmap({
+        stateStyles: {fill: '#5F5F5F'},
+        stateSpecificStyles: myStyles,
         click: function(event, data) {
             $('#clicked-state')
             .text('You clicked: ' + data.name);
@@ -19,9 +41,7 @@ $(document).ready(function() {
             console.log(data.name);
         }
     });
-
-    console.log("Entered!")
-});
+}
 
 function updateChart() {
     var $image = $("img").first();
@@ -32,3 +52,54 @@ function updateChart() {
         $image.attr("src", "../site/img/chart02.jpg")
     )
 }
+
+function workOutValues(percent) 
+{
+    percent = percent/100
+    if(percent<=-0.5)
+    {
+        if(255-(-percent-0.5)*510<16)
+        {
+            return "#"+Math.round(255).toString(16)+"0"+Math.round(255-(-percent-0.5)*510).toString(16)+"0"+Math.round(0).toString(16)
+        }
+        else
+        {
+            return "#"+Math.round(255).toString(16)+Math.round(255-(-percent-0.5)*510).toString(16)+"0"+Math.round(0).toString(16)
+        }
+    }
+    if(percent<=0)
+    {
+        if((-percent)*510<16)
+        {
+            return "#0"+Math.round((-percent)*510).toString(16)+Math.round(255).toString(16)+"0"+Math.round(0).toString(16)
+        }
+        else
+        {
+            return "#"+Math.round((-percent)*510).toString(16)+Math.round(255).toString(16)+"0"+Math.round(0).toString(16)
+        }
+    }
+    if(percent<=0.5)
+    {
+        if((percent)*510<16)
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255).toString(16)+"0"+Math.round((percent)*510).toString(16)
+        }
+        else
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255).toString(16)+Math.round((percent)*510).toString(16)
+        }
+    }
+    if(percent<=1)
+    {
+        if(255-(percent-0.5)*510<16)
+        {
+            return "#0"+Math.round(0).toString(16)+"0"+Math.round(255-(percent-0.5)*510).toString(16)+Math.round(255).toString(16)
+        }
+        else
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255-(percent-0.5)*510).toString(16)+Math.round(255).toString(16)
+        }
+    }
+}
+
+
