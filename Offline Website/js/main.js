9535f229b96ef5e1b44eb558ab2d68479161a2cf
emuu1802 <michaelworthington18@gmail.com>
2018-04-26 20:54:20 +0100

    Added buttons & colour
    Mike - added buttons for ba's
    Rob - added colour to map

--- Offline Website/js/main.js
+++ Offline Website/js/main.js
@@ -1,15 +1,40 @@
-$(document).ready(function() {
-    $('#map').usmap({
-        stateStyles: {fill: '#95ccff'},
-        stateHoverStyles: {fill: '#e0f4f2'},
-        stateHoverAnimation: 100,
 
+function parseData2() {
+    Papa.parse("../data/stateColours.csv", {
+        download: true,
+        complete: function(results) {
+            var resultsData = results.data
+            printMap(resultsData)
+        }
+    })
+}
+$(document).ready(function() {
+    parseData2()
+    console.log("Entered!")
+});
+
+function printMap(data)
+{
+    // Array to object.
+    var oldClass = ""
+    var myStyles = {}
+    for (var i = 1; i < data.length; i++) {
+        myStyles[data[i][0]] = {fill: workOutValues(data[i][1])};
+    }
+
+    $('#map').usmap({
+        stateStyles: {fill: '#5F5F5F'},
+        stateSpecificStyles: myStyles,
         click: function(event, data) {
             $('#clicked-state')
             .text('You clicked: ' + data.name);
+            if(oldClass != "")
+                document.getElementById(oldClass).style.display = "none";
+            document.getElementById(data.name).style.display = "block";
+            oldClass = data.name;
             console.log(data.name);
             updateChart();
-            $('#map').usmap({stateStyles: {fill: 'red'}});
+
         },
 
         // the hover action
@@ -19,9 +44,7 @@ $(document).ready(function() {
             console.log(data.name);
         }
     });
-
-    console.log("Entered!")
-});
+}
 
 function updateChart() {
     var $image = $("img").first();
@@ -32,3 +55,71 @@ function updateChart() {
         $image.attr("src", "../site/img/chart02.jpg")
     )
 }
+
+function workOutValues(percent)
+{
+    percent = percent/100
+    if(percent<=-0.5)
+    {
+        if(255-(-percent-0.5)*510<16)
+        {
+            return "#"+Math.round(255).toString(16)+"0"+Math.round(255-(-percent-0.5)*510).toString(16)+"0"+Math.round(0).toString(16)
+        }
+        else
+        {
+            return "#"+Math.round(255).toString(16)+Math.round(255-(-percent-0.5)*510).toString(16)+"0"+Math.round(0).toString(16)
+        }
+    }
+    if(percent<=0)
+    {
+        if((-percent)*510<16)
+        {
+            return "#0"+Math.round((-percent)*510).toString(16)+Math.round(255).toString(16)+"0"+Math.round(0).toString(16)
+        }
+        else
+        {
+            return "#"+Math.round((-percent)*510).toString(16)+Math.round(255).toString(16)+"0"+Math.round(0).toString(16)
+        }
+    }
+    if(percent<=0.5)
+    {
+        if((percent)*510<16)
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255).toString(16)+"0"+Math.round((percent)*510).toString(16)
+        }
+        else
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255).toString(16)+Math.round((percent)*510).toString(16)
+        }
+    }
+    if(percent<=1)
+    {
+        if(255-(percent-0.5)*510<16)
+        {
+            return "#0"+Math.round(0).toString(16)+"0"+Math.round(255-(percent-0.5)*510).toString(16)+Math.round(255).toString(16)
+        }
+        else
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255-(percent-0.5)*510).toString(16)+Math.round(255).toString(16)
+        }
+    }
+}
+
+// $('.radioButton').click(function(this) {
+//     ($this).val()
+// });
+//
+// $('.radioButton').click(setShape);
+//
+// function setShape() {
+//     var BA  = $('.radionButton:checked').val();
+//     }
+//
+// {
+//     ($this).val()
+// });
+
+$('.radioButton').click(function() {
+    var BA  = $('.radioButton:checked').val();
+    console.log(BA + 'HI')
+});
