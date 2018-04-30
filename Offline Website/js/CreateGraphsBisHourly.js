0d6eadc9d0d2d1b75adb9c1928aba1319a3e2b9b
SurgeArrester <cameron.h@rgreaves.me.uk>
2018-04-30 21:10:43 +0100

    Merged and cleaned multiple files
    Added string parsing for selection
    Cleaned HTML for radio buttons
    Cleaned and made JS working
    Added CSS for prettiness
    Hid and showed various divs for usability

--- Offline Website/js/CreateGraphsBisHourly.js
+++ Offline Website/js/CreateGraphsBisHourly.js
@@ -2,78 +2,89 @@
  * Parse the data and create a graph with the data.
  */
 
-
-function parseData(createGraph) {
-    Papa.parse("../data/DUK_actual_hourly.csv", {
+function parseData(createGraph, parseParameters) {
+    let fileString = "../data/" + parseParameters['BA'] + "_hourly_" + parseParameters['timePrecision'] + ".csv"
+    // let fileString = "../data/DUK_hourly_Daily.csv"
+    console.log(fileString);
+    Papa.parse(fileString, {
         download: true,
         complete: function(results) {
-            createGraph(results.data);
+            createGraph(results.data, parseParameters);
         }
     });
 }
 
-function createGraph(data) {
+function createGraph(data, parseParameters) {
+    let startDate = [parseParameters['startDate'] + " 00:00"];
+    let endDate = [parseParameters['endDate'] + " 00:00"];
+    console.log(startDate);
+    console.log(endDate);
+    // let startDate = ["01/04/2018 00:00"];
+    // let endDate = ["14/04/2018 00:00"];
+    let b = false;
+    //let difference = [];
 
-    var startDate = ["01/07/2015 00:00"];
-    var endDate = ["11/04/2016 04:00"];
-    //var difference = [];
 
+    let time = [];
+    let Demand = ["Actual Demand "];
+    let Forecast = ["US Forecast"];
+    let Prediction = ["Our Prediction"];
+    let rf = ["RF Prediction"];
+    let mlp = ["MLP Prediction"];
+    let svm = ["SVM Prediction"];
 
-    var time = [];
-    var Demand = ["Predicted Demand "];
-    var Forecast = ["U.S Forecast"];
-    var Prediction = ["Our Prediction"];
-    var b = false;
-
-    for (var i = 1; i < data.length; i++) {
-
+    for (let i = 1; i < data.length; i++) {
        // _.range(startDate, endDate);
        // time.push(data[i][0]);
 
-        if (data[i][0] == startDate) {           
-            b = true; 
-            
-            }
+        if (data[i][0] == startDate) {
+            b = true;
+        }
 
         if (data[i][0] == endDate){
             time.push(data[i][0]);
             Demand.push(data[i][1]);
             Forecast.push(data[i][2]);
             Prediction.push(data[i][3]);
-            
-            b = false
-            break
-            
+            rf.push(data[i][4]);
+            mlp.push(data[i][5]);
+            svm.push(data[i][6]);
+            b = false;
+            break;
+
             }
-        
+
         if (b == true){
 
-        
         time.push(data[i][0]);
         Demand.push(data[i][1]);
         Forecast.push(data[i][2]);
         Prediction.push(data[i][3]);
-            }
- 
-       
-
+        rf.push(data[i][4]);
+        mlp.push(data[i][5]);
+        svm.push(data[i][6]);
+        }
     }
-   
 
-    console.log(time);
-    console.log(Demand);    
-    console.log(Forecast);
-    console.log(Prediction);
-   
+    // console.log(time);
+    // console.log(Demand);
+    // console.log(Forecast);
+    // console.log(Prediction);
+    // console.log(rf);
+    // console.log(mlp);
+    // console.log(svm);
 
-    var chart = c3.generate({
+    let chart = c3.generate({
             to: '#chart',
             data: {
-                columns: 
-                
-            [   Demand, 
-                Forecast, 
+                columns:
+
+            [   Demand,
+                Forecast,
                 Prediction,
+                rf,
+                mlp,
+                svm,
                 ]
             },
             axis: {
@@ -94,9 +105,7 @@ function createGraph(data) {
             legend: {
                 position: 'right'
             }
-      
+
         });
 
 }
-
-parseData(createGraph);
--- Offline Website/js/main.js
+++ Offline Website/js/main.js
@@ -1,34 +1,143 @@
-$(document).ready(function() {
-    $('#map').usmap({
-        stateStyles: {fill: '#95ccff'},
-        stateHoverStyles: {fill: '#e0f4f2'},
-        stateHoverAnimation: 100,
+var selectedState = "";
+var startDate = 0;
+var endDate = 0;
+var timePrecision = "";
+var parseParameters = {};
+var ba = ""
 
-        click: function(event, data) {
-            $('#clicked-state')
-            .text('You clicked: ' + data.name);
-            console.log(data.name);
-            updateChart();
-            $('#map').usmap({stateStyles: {fill: 'red'}});
-        },
-
-        // the hover action
-        mouseover: function(event, data) {
-            $('#clicked-state')
-            .text('You hovered: '+data.name);
-            console.log(data.name);
+function parseStateColorsAndUpdate() {
+    Papa.parse("../data/stateColours.csv", {
+        download: true,
+        complete: function(results) {
+            var resultsData = results.data
+            printMap(resultsData)
         }
+    })
+}
+
+$(document).ready(function() {
+    parseStateColorsAndUpdate()
+    $(function(){
+      $("#radioButtonContainer").load("radioButtons.html");
+    });
+    console.log("Entered!")
+
+    $( function() {
+        $( "#startDate").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            dateFormat: 'dd/mm/yy'});
     });
 
-    console.log("Entered!")
+    $( function() {
+        $( "#endDate").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            dateFormat: 'dd/mm/yy'});
+    });
 });
 
-function updateChart() {
-    var $image = $("img").first();
-    if ($image.attr("src") == "../site/img/chart01.jpg") {
-        $image.attr("src", "../site/img/chart02.jpg");
+function printMap(data)
+{
+    // Array to object.
+    var oldClass = ""
+    var myStyles = {}
+
+    for (var i = 1; i < data.length; i++) {
+        myStyles[data[i][0]] = {fill: workOutValues(data[i][1])};
     }
-    else (
-        $image.attr("src", "../site/img/chart02.jpg")
-    )
+
+    $('#map').usmap({
+        stateStyles: {fill: '#5F5F5F'},
+        stateSpecificStyles: myStyles,
+
+        click: function(event, data) {
+            $('#clicked-state').text('You have selected: ' + data.name);
+            selectedState = data.name;
+
+            if(oldClass != "") {
+                document.getElementById(oldClass).style.display = "none";
+            }
+            document.getElementById(data.name).style.display = "block";
+            oldClass = data.name;
+            console.log(data.name);
+        },
+    });
+};
+
+function workOutValues(percent)
+{
+    percent = percent/100
+    if(percent<=-0.5)
+    {
+        if(255-(-percent-0.5)*510<16)
+        {
+            return "#"+Math.round(255).toString(16)+"0"+Math.round(255-(-percent-0.5)*510).toString(16)+"0"+Math.round(0).toString(16)
+        }
+        else
+        {
+            return "#"+Math.round(255).toString(16)+Math.round(255-(-percent-0.5)*510).toString(16)+"0"+Math.round(0).toString(16)
+        }
+    }
+    if(percent<=0)
+    {
+        if((-percent)*510<16)
+        {
+            return "#0"+Math.round((-percent)*510).toString(16)+Math.round(255).toString(16)+"0"+Math.round(0).toString(16)
+        }
+        else
+        {
+            return "#"+Math.round((-percent)*510).toString(16)+Math.round(255).toString(16)+"0"+Math.round(0).toString(16)
+        }
+    }
+    if(percent<=0.5)
+    {
+        if((percent)*510<16)
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255).toString(16)+"0"+Math.round((percent)*510).toString(16)
+        }
+        else
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255).toString(16)+Math.round((percent)*510).toString(16)
+        }
+    }
+    if(percent<=1)
+    {
+        if(255-(percent-0.5)*510<16)
+        {
+            return "#0"+Math.round(0).toString(16)+"0"+Math.round(255-(percent-0.5)*510).toString(16)+Math.round(255).toString(16)
+        }
+        else
+        {
+            return "#0"+Math.round(0).toString(16)+Math.round(255-(percent-0.5)*510).toString(16)+Math.round(255).toString(16)
+        }
+    }
 }
+
+// Get data from button presses
+$('.button').click(function() {
+    console.log("Handler for .click called!");
+    startDate = $('#startDate').val();
+    endDate = $('#endDate').val();
+    timePrecision = $(this).val();
+    ba = $("input[name='myRadio']:checked").val();
+    parseParameters = {
+        'startDate' : startDate,
+        'endDate' : endDate,
+        'state' : selectedState,
+        'timePrecision' : timePrecision,
+        'BA' : ba
+        }
+    console.log(parseParameters);
+    console.log(Date.parse(startDate));
+    console.log(Date.parse(endDate));
+
+    parseData(createGraph, parseParameters);
+});
+
+$(document).on('change',"input[name='myRadio']:radio",function(){
+    ba = $("input[name='myRadio']:checked").val();
+    document.getElementById('dateContainer').style.display = "block";
+    document.getElementById('selectionButtonContainer').style.display = "block";
+    console.log(ba);
+});
